4b7215c8b13ee7df169bf1452dbc8e2a935628a7
adek312 <adrian.dolata@vp.pl>
2024-01-27 22:19:27 +0100

    add miliseconds to clock

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const container = document.querySelector(".container");
+const currentTimeContainer = document.querySelector(".currentTimeContainer");
+
+const currentTimeClock = document.createElement("div");
+const currentTimeText = document.createElement("div");
+currentTimeText.innerHTML = "Current Time:";
+currentTimeContainer.appendChild(currentTimeText);
+
 
 function updateClock() {
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+    let miliseconds = now.getMilliseconds();
   
     hours = hours < 10 ? '0' + hours : hours;
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
+    miliseconds = miliseconds < 10 ? '00' + miliseconds : (miliseconds < 100 ? '0' + miliseconds : miliseconds);
   
-    var time = hours + ':' + minutes + ':' + seconds;
+
+    let time = hours + ':' + minutes + ':' + seconds + ":" + miliseconds;
   
-    container.innerHTML = time;
-  
-    setTimeout(updateClock, 1000);
+    currentTimeClock.innerHTML = time;
+    currentTimeContainer.appendChild(currentTimeClock);
+    setTimeout(updateClock, 100);
   }
   
-  window.onload = updateClock;
+updateClock();
