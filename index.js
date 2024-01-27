d2162bedb52c65973adca497095b9684b701cd32
adek312 <adrian.dolata@vp.pl>
2024-01-27 15:19:48 +0100

    do basic clock

---
+++ index.js
@@ -0,0 +1,20 @@
+const container = document.querySelector(".container");
+
+function updateClock() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+  
+    hours = hours < 10 ? '0' + hours : hours;
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+  
+    var time = hours + ':' + minutes + ':' + seconds;
+  
+    container.innerHTML = time;
+  
+    setTimeout(updateClock, 1000);
+  }
+  
+  window.onload = updateClock;
